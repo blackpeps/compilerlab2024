2cd1fec3397c8ff128138c615d12b73879dbc111
blackpeps <126700907+blackpeps@users.noreply.github.com>
2024-11-01 02:19:19 +0530

    Update exp17.c

--- Code/EXP17/exp17.c
+++ Code/EXP17/exp17.c
@@ -7,19 +7,12 @@ char op[2], arg1[5], arg2[5], result[5];
 
 int main()
 {
-
     FILE *fp1, *fp2;
     fp1 = fopen("input.txt", "r");
-    if (fp1 == NULL)
-    {
-        printf("Error opening input file.\n");
-        return 1;
-    }
     fp2 = fopen("output.txt", "w");
-    if (fp2 == NULL)
+    if (fp1 == NULL || fp2 == NULL)
     {
-        printf("Error opening output file.\n");
-        fclose(fp1);
+        printf("Error opening input or output file.\n");
         return 1;
     }
 
@@ -47,6 +40,11 @@ int main()
         {
             fprintf(fp2, "\n MOV R0,%s", arg1);
             fprintf(fp2, "\n DIV R0,%s", arg2);
+            fprintf(fp2, "\n MOV %s,R0", result);
+        }
+        else if (strcmp(op, "=") == 0)
+        {
+            fprintf(fp2, "\n MOV R0,%s", arg1);
             fprintf(fp2, "\n MOV %s,R0", result);
         }
     }
